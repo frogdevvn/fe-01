4217d9ce89c88d510e4297611a9a221d3bc01d1f
Long Dang <frog.dev.studio@gmail.com>
2022-09-21 00:49:06 +0700

    feat: add hover color for top nav

--- src/components/top-nav/index.tsx
+++ src/components/top-nav/index.tsx
@@ -6,10 +6,37 @@ const TopNav = () => {
     <div className="flex justify-between">
       <LogoIcon />
       <nav className="typo-sub-heading flex items-center gap-x-8 text-white-100">
-        <Link href="/">Home</Link>
-        <Link href="/portfolio">Portfolio</Link>
-        <Link href="/services">Services</Link>
-        <Link href="/contact">Contact</Link>
+        <Link href="/">
+          <a>
+            <p className="typo-sub-heading font-normal hover:text-dark-25">
+              Home
+            </p>
+          </a>
+        </Link>
+
+        <Link href="/portfolio">
+          <a>
+            <p className="typo-sub-heading font-normal hover:text-dark-25">
+              Portfolio
+            </p>
+          </a>
+        </Link>
+
+        <Link href="/services">
+          <a>
+            <p className="typo-sub-heading font-normal hover:text-dark-25">
+              Services
+            </p>
+          </a>
+        </Link>
+
+        <Link href="/contact">
+          <a>
+            <p className="typo-sub-heading font-normal hover:text-dark-25">
+              Contact
+            </p>
+          </a>
+        </Link>
       </nav>
     </div>
   );
